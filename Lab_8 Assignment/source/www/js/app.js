dd83f92dc2d48ee4b5c76dc8a97c22894b9efe31
maninandan <manusani.kumar.s@gmail.com>
2016-10-19 21:32:28 -0700

    commit

---
+++ Lab_8 Assignment/source/www/js/app.js
@@ -0,0 +1,60 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+<<<<<<< HEAD
+=======
+      .controller('View1Ctrl', function ($scope, $http) {
+        $scope.venueList = new Array();
+        $scope.mostRecentReview;
+        $scope.getVenues = function () {
+            var placeEntered = document.getElementById("txt_placeName").value;
+            var searchQuery = document.getElementById("txt_searchFilter").value;
+            if (placeEntered != null && placeEntered != "" && searchQuery != null && searchQuery != "") {
+                document.getElementById('div_ReviewList').style.display = 'none';
+                //This is the API that gives the list of venues based on the place and search query.
+                var handler = $http.get("https://api.foursquare.com/v2/venues/search" +
+                    "?client_id=1WNTPJ4CENC4KPEBYOJXVHO2BJT3C34550AZ1JHJN22O05YB" +
+                    "&client_secret=N0BIUF0OUUO13OBN0BMERCHUP40GDMBIRSMD5WLU3YHXQ2IO" +
+                    "&v=20160215&limit=5" +
+                    "&near=" + placeEntered +
+                    "&query=" + searchQuery);
+                handler.success(function (data) {
+
+                    if (data != null && data.response != null && data.response.venues != undefined && data.response.venues != null) {
+                        for (var i = 0; i < data.response.venues.length; i++) {
+                            $scope.venueList[i] = {
+                                "name": data.response.venues[i].name,
+                                "id": data.response.venues[i].id,
+                                "location": data.response.venues[i].location
+                            };
+                        }
+                    }
+
+                })
+                handler.error(function (data) {
+                    alert("There was some error processing your request. Please try after some time.");
+                });
+            }
+        }
+	 });
+>>>>>>> origin/master
+})
